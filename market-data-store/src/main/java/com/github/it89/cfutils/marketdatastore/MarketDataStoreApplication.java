a9ee415a17296039c749affe812258538919da26
it89 <ivan.tikhonov.spb@gmail.com>
2022-03-14 20:49:25 +0300

    Generate Spring Boot project

---
+++ market-data-store/src/main/java/com/github/it89/cfutils/marketdatastore/MarketDataStoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.it89.cfutils.marketdatastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDataStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketDataStoreApplication.class, args);
+	}
+
+}
